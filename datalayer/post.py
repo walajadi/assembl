063dbbebc04e31db5ac3a736793e3e2f5f6718ce
slim <=>
2017-09-17 17:23:17 +0200

    insert idea to post

--- datalayer/post.py
+++ datalayer/post.py
@@ -10,11 +10,19 @@ class Post(Document):
     title = StringField()
     is_top = BooleanField()
     name = StringField()
-    topics = ListField(StringField)
+    topics = ListField(StringField())
     created = DateTimeField(default=datetime.now())
     author = StringField()  # user_id
     content = StringField()
     likes = IntField()
-    ideas = ListField(StringField)
+    ideas = ListField(StringField())
     parent = StringField()  # post_id as str, empty if is_top
 
+    def add_idea(self, idea):
+        """
+        Add idea to post.
+        :param idea:
+        :return:
+        """
+        self.ideas.append(str(idea.id))
+        self.save()
