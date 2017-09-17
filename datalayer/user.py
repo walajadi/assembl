e29d0fad8b2d64a211a3dd0d058e38d12ac12016
slim <=>
2017-09-17 11:08:15 +0200

    refine user class with correct fields

--- datalayer/user.py
+++ datalayer/user.py
@@ -1,22 +1,14 @@
 
 from mongoengine import connect, Document, ListField, StringField, BooleanField, DateTimeField, IntField, EmailField,\
-    UrlField
+    URLField
 
 connect('assembl')
 
 
 class User(Document):
-
-    username = StringField()
-    email = EmailField()
-    password = StringField()
+    username = StringField(unique=True)
+    email = EmailField(unique=True)
+    password = StringField()  # add encryption.
     created = DateTimeField()
     verified = BooleanField()
-    avatar = UrlField()
-
-    meta = {
-        'db_alias': 'assembl'
-    }
-
-
-
+    avatar = URLField()
