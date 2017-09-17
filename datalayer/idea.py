d0d3584e97072fb1abf737318a3a3147f7535a19
slim <=>
2017-09-17 11:09:29 +0200

    create idea class and add children and authorized users

--- datalayer/idea.py
+++ datalayer/idea.py
@@ -1,18 +1,19 @@
 
-from mongoengine import connect, Document, ListField, StringField, BooleanField, DateTimeField, IntField
+from datetime import datetime
+from mongoengine import connect, Document, ListField, StringField, BooleanField, DateTimeField, IntField, DictField
+
 
 connect('assembl')
 
 
 class Idea(Document):
 
-    title = StringField()
-    description = StringField()
-    created = DateTimeField()
+    title = StringField(required=True)
+    description = StringField(required=True)
+    created = DateTimeField(default=datetime.now())
     updated = DateTimeField()
     deleted = DateTimeField()
-    post = StringField()  # post id
-
-    meta = {
-        'db_alias': 'assembl'
-    }
+    posts = ListField(StringField())  # post id associated to this idea
+    creator = StringField()  # user_id as str
+    children_ideas = ListField(StringField())  # idea_ids list
+    authorized_users = DictField()
